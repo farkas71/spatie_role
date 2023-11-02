ae6d82cd6f6663dcac7e877468cc29a25af8dacf
farkas71 <farkasmisi1222@gmail.com>
2023-11-02 10:16:10 +0100

    dashboard oldal jogosultságokkal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,28 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    @if(auth()->user()->hasRole('super_admin'))
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+        </x-slot>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("A SUPERADMIN MINDENT LÁTHAT") }}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    @else
+       <div class="container mt-5">
+            <div class="alert alert-danger text-center" role="alert">
+                <h2>Nem jogosult a taretalom megtekintésére!</h2>
+            </div>
+        </div>
+        @endif
+        
+    </x-app-layout>
+
